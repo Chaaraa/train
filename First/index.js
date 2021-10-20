810c02ccf0b7d445de1f91b3335e18d8fbc3bf0c
alina <alinamackarova22@gmail.com>
2021-10-20 10:17:44 +0300

    second

--- First/index.js
+++ First/index.js
@@ -3,6 +3,29 @@ let button = document.getElementById('button')
 let div = document.getElementById('div')
 let visible=document.getElementById('visible')
 let sum = 0
+let arr = []
+let utro1 = document.getElementById('utro1')
+let utro2= document.getElementById('utro2')
+
+
+utro1.addEventListener('click', Utro1)
+utro2.addEventListener('click',Utro2)
+
+function Utro1() {
+    sum = 0
+    for (let i = 1; i < 5; i++) {
+        sum += arr[i]
+        div.innerHTML = sum
+    }
+}
+
+function Utro2(){
+    sum = 1
+        for (let i = 1; i < 5; i++) {
+            sum *= arr[i]
+            div.innerHTML = sum
+        }
+}
 
 // button.addEventListener('click',Click)
 //
@@ -25,7 +48,13 @@ sumButton.addEventListener('click',Sum)
 function Sum() {
     for (let i = 1; i < 5; i++) {
         let input = document.getElementById(`input${i}`)
-        sum += +input.value
-        div.innerHTML = sum
+        arr[i] = +input.value
+        if(arr[i]%2===1){
+            console.log(arr[i])
+        }
     }
+    console.log(arr)
+    /*div.innerHTML = arr[]*/
 }
+
+
